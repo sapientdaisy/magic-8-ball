b01aa3bef91fcbf6f80217eb3acf4530adf7694d
Sapient Daisy <susie@sapientdaisy.com>
2017-08-11 11:13:55 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+  $("#answer").hide();
+
+var magic8Ball = {};
+
+magic8Ball.listOfAnswers = ["Ask again later",
+"Don’t count on it", "Totally", "Not feelin' it", "Reply hazy try again", "Yep"];
+
+
+magic8Ball.giveAnswer =function(question){
+
+	var randomNumber = Math.random();
+	var randomArray = randomNumber * this.listOfAnswers.length;
+	var randomIndex = Math.floor(randomArray);
+	var answer = this.listOfAnswers[randomIndex];
+
+  $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png");
+
+	$("#answer").text( answer );
+  $("#answer").fadeIn(4000);
+
+  }
+
+//Ask me a question button
+magic8Ball.askQuestion = function (){
+      $("#answer").hide();
+      $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/8side.png");
+      setTimeout(function() {
+        prompt("ASK A YES/NO QUESTION");
+        $("#8ball").effect("shake");
+        $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+        magic8Ball.giveAnswer();
+       }, 500);
+			};
+    $("#questionButton").click(magic8Ball.askQuestion);
+
+  });
